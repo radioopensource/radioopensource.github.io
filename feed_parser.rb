e852d58d6f01e5c278440b10a4285c61794ca109
Eve Asher <eve.adele.asher@gmail.com>
2015-02-19 13:45:40 -0500

    initial commit

---
+++ feed_parser.rb
@@ -0,0 +1,29 @@
+require 'feedjira'
+require 'nokogiri'
+
+module FeedParser
+  URLS = {
+    "NPR" => "http://www.npr.org/rss/podcast.php?id=500005",
+    "BBC" => "http://downloads.bbc.co.uk/podcasts/worldservice/globalnews/rss.xml",
+    "PRI" => "http://feeds.feedburner.com/pri/theworld?format=xml"
+  }
+
+  def most_recent_items
+    result = {}
+
+    URLS.each do |k, v|
+      result[k] = grab_most_recent_from(k, v)
+    end
+
+    result
+  end
+
+  def grab_most_recent_from(source, url)
+    if source == "PRI"
+      xml = Feedjira::Feed.fetch_raw(url)
+      Nokogiri::XML(xml).at_css("item enclosure").attributes["url"].value
+    else
+      Feedjira::Feed.fetch_and_parse(url).entries.first.enclosure_url
+    end
+  end
+end
